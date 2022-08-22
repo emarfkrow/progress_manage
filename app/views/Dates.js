8fcd32755e89aee4d1c9b58cebba056ca89bd093
ktc-fukuo <t_fukuo@kyototool.co.jp>
2022-08-22 14:43:57 +0900

    OutOfMemory対応

---
+++ app/views/Dates.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+ 
+ var Dates = {
+	
+	parseYyyyHmmHdd: function(yyyyHmmHdd) {
+		if (!yyyyHmmHdd) {
+			return null;
+		}
+		var ymds = yyyyHmmHdd.split('-');
+		return new Date(ymds[0] * 1, ymds[1] - 1, ymds[2]);
+	},
+
+	parseYySmmSdd: function(yySmmSdd) {
+		if (!yySmmSdd) {
+			return null;
+		}
+		var ymds = yySmmSdd.split('/');
+		return new Date(2000 + ymds[0] * 1, ymds[1] - 1, ymds[2]);
+	},
+
+	formatYyyyHmmHdd: function(date) {
+		if (!date) {
+			return '';
+		}
+		var yyyy = date.getFullYear();
+		var mm = ('0' + (date.getMonth() + 1)).slice(-2);
+		var dd = ('0' + date.getDate()).slice(-2);
+		return yyyy + '-' + mm + '-' + dd;
+	},
+
+	formatYyHmmHdd: function(date) {
+		if (!date) {
+			return '';
+		}
+		var yyyy = date.getFullYear();
+		var yy = yyyy - 2000;
+		var mm = ('0' + (date.getMonth() + 1)).slice(-2);
+		var dd = ('0' + date.getDate()).slice(-2);
+		return yy + '/' + mm + '/' + dd;
+	},
+}
