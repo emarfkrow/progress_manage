e9c580828c642b7f912589010f47493c4e7f4b3c
ktc-fukuo <t_fukuo@kyototool.co.jp>
2022-08-24 10:42:24 +0900

    データをajax化

--- app/controllers/progress_manage_controller.rb
+++ app/controllers/progress_manage_controller.rb
@@ -1,5 +1,21 @@
 class ProgressManageController < ApplicationController
 
+  #
+  # 初期表示
+  #
   def index
   end
+
+  def holidays
+  end
+
+  def statuses
+  end
+
+  def users
+  end
+
+  def search
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+get 'progress_manage',          :to => 'progress_manage#index'
+get 'progress_manage/holidays', :to => 'progress_manage#holidays'
+get 'progress_manage/statuses', :to => 'progress_manage#statuses'
+get 'progress_manage/users',    :to => 'progress_manage#users'
+get 'progress_manage/search',   :to => 'progress_manage#search'
