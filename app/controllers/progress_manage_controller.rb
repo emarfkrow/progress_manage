e6f8d26fc3ef085f7a5a5cb10bc71095b66a56ef
ktc-fukuo <t_fukuo@kyototool.co.jp>
2022-08-25 09:21:34 +0900

    照会機能を仮組

--- app/controllers/progress_manage_controller.rb
+++ app/controllers/progress_manage_controller.rb
@@ -7,11 +7,35 @@ class ProgressManageController < ApplicationController
     end
 
     def holidays
-        @actualHolidays = ActualHoliday.all
-        render json: @actualHolidays
+    
+    	jsonText = ""
+        actualHolidays = ActualHoliday.all
+        actualHolidays.each{|actualHoliday|
+            if jsonText == ""
+                jsonText += "{"
+            else
+                jsonText += ","
+            end
+            jsonText += "\"" + actualHoliday.holiday + "\":\"1\""
+		}
+    
+        render json: JSON.parse(jsonText)
     end
 
     def statuses
+    
+    	jsonText = ""
+        statuses = Status.all
+        statuses.each{|status|
+            if jsonText == ""
+                jsonText += "{"
+            else
+                jsonText += ","
+            end
+            jsonText += "\"" + status.id + "\":\"" + status.name + "\""
+		}
+    
+        render json: JSON.parse(jsonText)
     end
 
     def users
@@ -32,6 +56,27 @@ class ProgressManageController < ApplicationController
     end
 
     def search
+    
+    	jsonText = ""
+        actualSpans = Issue
+			.joins("INNER JOIN issue_statuses ist on ist.id = issues.status_id ")
+			.joins("LEFT JOIN actual_spans as on as.issue_id = issues.id")
+			.select("issues.*, actual_spans.*")
+			.where(["ist.is_closed = :closed", {:closed => false}])
+			.all
+        actualSpans.each{|actualSpan|
+            if jsonText == ""
+                jsonText += "{"
+            else
+                jsonText += ","
+            end
+            jsonText += "\"" + actualSpan.issue_id + "\":{"
+            jsonText += "\"id\":\"" + actualSpan.issue_id + "\","
+            jsonText += "\"eoDays\":\"" + actualSpan.eoDays + "\""
+            jsonText += "}"
+		}
+    
+        render json: JSON.parse(jsonText)
     end
 
 end
